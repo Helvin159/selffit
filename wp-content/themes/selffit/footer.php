5ee25331b7c36ea59a1f1a53ce175b929f1b928d
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-13 19:20:50 -0400

    rellinks

--- wp-content/themes/selffit/footer.php
+++ wp-content/themes/selffit/footer.php
@@ -1,7 +1,7 @@
 <!-- Image and text -->
 <nav class="navbar navbar-light" style="background-color:#1c3a13; position:absolute;width:100vw">
   <a class="navbar-brand" href="index.php" style="color:#ffffff;">
-    <img src="<?php echo get_theme_file_uri('Assets/img/favicon1.png')?>" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+    <!-- <img src="<?php //echo get_theme_file_uri('Assets/img/favicon1.png')?>" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy"> -->
     Self-Fit
   </a>
   <a href="<?php echo admin_url()?>" style="color:#ffffff;"><i class="fas fa-user-shield"></i></a>
