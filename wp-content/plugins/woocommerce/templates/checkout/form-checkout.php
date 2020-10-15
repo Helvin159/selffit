77407403c88efc907ec35e152abc047c7a2a1074
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-15 18:31:20 -0400

    css updates

--- wp-content/plugins/woocommerce/templates/checkout/form-checkout.php
+++ wp-content/plugins/woocommerce/templates/checkout/form-checkout.php
@@ -33,26 +33,26 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 	<?php if ( $checkout->get_checkout_fields() ) : ?>
 
-	<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
+		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
-	<div class="col2-set" id="customer_details">
-		<div class="col-1">
-			<?php do_action( 'woocommerce_checkout_billing' ); ?>
+		<div class="col2-set" id="customer_details">
+			<div class="col-1">
+				<?php do_action( 'woocommerce_checkout_billing' ); ?>
+			</div>
+
+			<div class="col-2">
+				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
+			</div>
 		</div>
 
-		<div class="col-2">
-			<?php do_action( 'woocommerce_checkout_shipping' ); ?>
-		</div>
-	</div>
-
-	<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
+		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 
 	<?php endif; ?>
-
+	
 	<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
-
+	
 	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
-
+	
 	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
 	<div id="order_review" class="woocommerce-checkout-review-order">
