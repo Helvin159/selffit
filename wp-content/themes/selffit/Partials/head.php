952fc99b423dff44dee035af4ef36cfd5a061798
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-17 10:09:02 -0400

    updates

--- wp-content/themes/selffit/Partials/head.php
+++ wp-content/themes/selffit/Partials/head.php
@@ -7,6 +7,16 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- Optimal Internet Explorer compatibility -->
 
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-149523138-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-149523138-2');
+</script>
+
 
 	<?php wp_head(); ?>
 </head>
