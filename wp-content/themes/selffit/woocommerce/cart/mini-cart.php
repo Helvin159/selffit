77345de6f1f1df98d02efdf5cec1c8b98e71a0f0
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-17 21:40:57 -0400

    categories page

--- wp-content/themes/selffit/woocommerce/cart/mini-cart.php
+++ wp-content/themes/selffit/woocommerce/cart/mini-cart.php
@@ -89,7 +89,7 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
 
 <?php else : ?>
 
-	<p class="woocommerce-mini-cart__empty-message"><?php esc_html_e( 'No hay productos en el carrito.', 'woocommerce' ); ?></p>
+	<p class="woocommerce-mini-cart__empty-message"><?php esc_html_e( 'No hay productos en el cart.', 'woocommerce' ); ?></p>
 
 <?php endif; ?>
 
