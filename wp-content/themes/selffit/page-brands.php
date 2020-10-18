b56fdf447822f10649ba3e3bc030d351d0ffe0da
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-18 10:49:48 -0400

    updates

---
+++ wp-content/themes/selffit/page-brands.php
@@ -0,0 +1,24 @@
+<?php 
+get_header();
+require_once('Partials/banner.php');
+?>
+<div class="row storePage">
+  
+  <div class="col-md-3 d-none d-sm-block <?php //if ($_SERVER['REQUEST_URI'] == '/product' ){ echo 'd-none';}?> px-3 widget" >
+    <?php echo get_sidebar()?>
+  </div>
+  <div class="col-md-9">
+    <div class="container">
+    <h1 class="py-5"><?php the_title() ?></h1>
+  </div>
+    <?php 
+     the_content();
+    ?> 
+  </div>
+</div>
+               
+
+<?php 
+require_once('Partials/rellinks.php');
+get_footer();
+?>
