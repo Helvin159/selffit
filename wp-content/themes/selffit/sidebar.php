2f3c9b4fd3e730907f9df3147fc3150bdf2a4edd
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-17 22:45:36 -0400

    categories page

--- wp-content/themes/selffit/sidebar.php
+++ wp-content/themes/selffit/sidebar.php
@@ -1,4 +1,8 @@
-<div class="container my-5"><?php dynamic_sidebar('side-bar'); ?></div>
+<?php 
+    if($_SERVER['REQUEST_URI'] != '/categories/'){ ?>
+        <div class="container my-5"><?php dynamic_sidebar('side-bar'); ?></div>
+    <?php }
+?>
 <div class="container my-5"><?php dynamic_sidebar('price-bar'); ?></div>
 <div class="container my-5"><?php dynamic_sidebar('blog-bar'); ?></div>
 <div class="container my-5"><?php dynamic_sidebar('tags-bar'); ?></div>
