83aaef2b6b7674367f1f2d4d1f77bb310dd141db
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-17 21:40:25 -0400

    categories page

--- wp-content/themes/selffit/page-categories.php
+++ wp-content/themes/selffit/page-categories.php
@@ -2,18 +2,19 @@
 get_header();
 require_once('Partials/banner.php');
 ?>
-
-<div class="container-fluid p-5 storePage">
-  <div class="container py-5"> 
-    <h1>
-      <?php 
-        the_title();
-      ?>
-    </h1>
-</div>
+<div class="row storePage">
+  
+  <div class="col-md-3 d-none d-sm-block <?php //if ($_SERVER['REQUEST_URI'] == '/product' ){ echo 'd-none';}?> px-3 widget" >
+    <?php echo get_sidebar()?>
+  </div>
+  <div class="col-md-9">
+    <div class="container">
+    <h1 class="py-5"><?php the_title() ?></h1>
+  </div>
  <?php 
                      the_content();
                     ?> 
+  </div>
 </div>
                
 
