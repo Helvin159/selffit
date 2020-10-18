14118d916f13b40ec3d5b759c1bb482576f3608b
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-17 20:42:53 -0400

    analytics

---
+++ wp-content/themes/selffit/page-categories.php
@@ -0,0 +1,25 @@
+<?php 
+get_header();
+require_once('Partials/banner.php'); 
+?>
+
+<div class="container-fluid p-5 storePage">
+  <div class="container py-5"> 
+    <h1>
+      <?php 
+        the_title();
+      ?>
+    </h1>
+  </div>
+      <div class="container text-center m-auto">
+        <?php 
+            the_content()
+        ?>
+      </div>
+</div>
+
+
+<?php 
+require_once('Partials/rellinks.php');
+get_footer();
+?>
