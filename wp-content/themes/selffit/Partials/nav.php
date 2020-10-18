6df67d218a7bf9b7f244247981a33202205fea79
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-18 11:51:15 -0400

    updates

--- wp-content/themes/selffit/Partials/nav.php
+++ wp-content/themes/selffit/Partials/nav.php
@@ -1,19 +1,9 @@
-<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container">
-    <a class="navbar-brand" href="index.php">Logo</a>
-  </div>
-</nav> -->
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#E9E9EA;">
   <a class="navbar-brand" href="<?php echo esc_url(site_url(''))?>">Self-Fit</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
-    <!-- <ul class="navbar-nav"> -->
-      <!-- <li class="nav-item active">
-        <a class="nav-link" href="<?php echo esc_url(site_url(''))?>">Home <span class="sr-only">(current)</span></a>
-      </li> -->
-
       <?php 
 						wp_nav_menu(array(
               'theme_location' => 'headerNav',
