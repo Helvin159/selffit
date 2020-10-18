f88ea25617d1bfc0b8ec34eb09e421a2e5ba431a
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-18 13:15:08 -0400

    updates

--- wp-content/themes/selffit/Partials/head.php
+++ wp-content/themes/selffit/Partials/head.php
@@ -1,7 +1,7 @@
 <head>
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
-	<meta name="Keywords" content="Fitness, self fit, fit, nutricion, ejercisio, cuerpo, musculo, musculos, motivación, dedicación, dominican, republic, dominican republic, bonao, selffit, real fitness bonao, perdida de peso, ganar peso, peso, supplements, twinlab, muscletech, signature, gnc, carbomax, weight loss, natural, remedies">
+	<meta name="Keywords" content="Fitness, self fit, fit, nutricion, ejercisio, cuerpo, musculo, musculos, motivación, dedicación, dominican, republic, dominican republic, bonao, selffit, real fitness bonao, perdida de peso, ganar peso, peso, supplements, twinlab, muscletech, signature, gnc, carbomax, weight loss, natural, remedies, creatine, creatina, destacado, self, whey">
 	<meta name="Description" content="Self-Fit Where You Get The Best!">
 	<meta name="Author" content="Broker">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
