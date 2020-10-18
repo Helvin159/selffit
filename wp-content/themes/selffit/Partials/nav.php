7c2eb2376bd35adbac4300538a35f169ec3de6b4
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-18 12:33:28 -0400

    nav update

--- wp-content/themes/selffit/Partials/nav.php
+++ wp-content/themes/selffit/Partials/nav.php
@@ -9,7 +9,7 @@
               'theme_location' => 'headerNav',
               // 'items_wrap' => false,
               'container' => false,
-              'menu_class' => 'navbar-nav'
+              'menu_class' => 'navbar-nav my-3'
 						));
 					?>
         <a href="<?php echo esc_url(site_url('/cart'))?>" class="cartIcon active d-inline ">
