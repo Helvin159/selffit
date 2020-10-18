1d22a9603d247e583828491b0020101b3e381685
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-18 17:53:18 -0400

    updates

--- wp-content/themes/selffit/index.php
+++ wp-content/themes/selffit/index.php
@@ -14,11 +14,11 @@ while($showPost->have_posts()){
       <div class="col-md-4 blogPost">
         <img class="img-fluid" alt="<?php the_title()?>" src="<?php the_post_thumbnail_url('blogSquare')?>" alt="" >
       </div>
-      <div class="col-md-8">
+      <div class="col-md-8 blogContent">
         <div class="container p-2">
-          <h3><a href="<?php the_permalink()?>"><?php the_title()?></a></h3>
+          <h3 class="py-3"><a  href="<?php the_permalink()?>"><?php the_title()?></a></h3>
           <div class="container">
-            <?php echo substr(get_the_excerpt(),0,425);echo '...';?>
+            <?php echo substr(get_the_excerpt(),0,425);?>
             <br>
             <div class="container my-1">
               <a href="<?php the_permalink()?>"> Ver Entrada Completa <i class="fas fa-long-arrow-alt-right"></i></a>              
