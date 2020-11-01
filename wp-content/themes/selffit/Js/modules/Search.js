7aeb68ad0e3670fba9f7716c23387ca144ae40f9
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-11-01 13:26:47 -0500

    updates

--- wp-content/themes/selffit/Js/modules/Search.js
+++ wp-content/themes/selffit/Js/modules/Search.js
@@ -46,7 +46,7 @@ class Search{
     }
 
     getResults(){
-        $.getJSON('https://selffit.com/wp-json/wp/v2/product?search=' + this.searchBar.val(), (result) => {
+        $.getJSON('https://selffitdr.com/wp-json/wp/v2/product?search=' + this.searchBar.val(), (result) => {
         this.resultsDiv.html(`
             <div class="container px-5 py-3">
                 <h1>Resultados</h1>
